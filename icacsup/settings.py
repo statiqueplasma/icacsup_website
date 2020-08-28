441065b2e4ba333e527a1c43b567d1c88a5289ea
statiqueplasma <h.aouadidrissi@gmail.com>
2020-08-28 23:14:48 +0200

    nothin

--- icacsup/settings.py
+++ icacsup/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 #EMAIL_HOST = 'localhost'
 #EMAIL_PORT = '1025'
