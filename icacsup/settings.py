b0f8dc8c69e92fee03bda364f25aa93e5d8b6e16
statiqueplasma <h.aouadidrissi@gmail.com>
2020-08-28 23:32:05 +0200

    tired

--- icacsup/settings.py
+++ icacsup/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic'
+    #'whitenoise.runserver_nostatic'
     'django.contrib.staticfiles',
 
     'website',
