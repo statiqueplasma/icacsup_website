2b36539d59cb20c2ea2f892fd453a527fc986fee
statiqueplasma <70385849+statiqueplasma@users.noreply.github.com>
2020-09-27 01:46:03 +0200

    Update settings.py

--- icacsup/settings.py
+++ icacsup/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path, os
 #import django_heroku
 #import dj_database_url
-from decouple import config
+#from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
